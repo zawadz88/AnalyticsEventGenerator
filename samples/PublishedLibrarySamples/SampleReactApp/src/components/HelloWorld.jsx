6f93fbc18cb733e3d84709613c76ee5f1d5de62d
Piotr Zawadzki <piotr@zawadzki.dev>
2024-01-30 20:47:06 +0100

    Add Hello World React application with Webpack

---
+++ samples/PublishedLibrarySamples/SampleReactApp/src/components/HelloWorld.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+const HelloWorld = () => (
+  <div className="hello-world">
+    <h1>Hello World</h1>
+  </div>
+);
+export default HelloWorld;
---
+++ samples/PublishedLibrarySamples/SampleReactApp/webpack.config.js
@@ -0,0 +1,47 @@
+const path = require('path');
+
+module.exports = {
+    entry: path.resolve(__dirname, 'src', 'index.jsx'),
+    output: {
+        path: path.resolve(__dirname, 'output'),
+        filename: 'bundle.js'
+    },
+    resolve: {
+        extensions: ['.js', '.jsx'],
+        alias: {
+           components: path.resolve(__dirname, 'src', 'components'),
+           environment: path.resolve(__dirname, 'src', 'environment'),
+           styles: path.resolve(__dirname, 'src', 'styles'),
+        },
+    },
+   devServer: {
+       static: [
+          {
+             directory: path.resolve(__dirname, 'src'),
+          },
+          {
+             directory: path.join(__dirname, 'output'),
+             publicPath: '/output'
+          }
+       ]
+   },
+   module: {
+       rules: [
+          {
+             test: /\.jsx$/,
+             use: {
+                loader: 'babel-loader',
+                options: {
+                   presets: ['@babel/preset-react', '@babel/preset-env']
+                }
+             }
+          },
+          {
+             test: /\.scss$/,
+             use: [
+                'style-loader', 'css-loader', 'postcss-loader', 'sass-loader'
+             ]
+          }
+       ]
+    }
+}
